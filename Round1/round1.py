39926b0878683b493de28dfb7aebda5eae703e23
Manindra de Mel <manindrademel@yahoo.com.au>
2025-04-09 00:21:28 +1000

    integrate the backtester

--- Round1/round1.py
+++ Round1/round1.py
@@ -213,8 +213,7 @@ class Trader:
             trade_volume = max(best_bid,sell_capacity) 
             if trade_volume > 0:
                 orders.append(Order(product,best_bid,-trade_volume))
-                summary = f"LIMIT SELL {int(trade_volume)} {product} at {be
-                                                                         st_bid} "
+                summary = f"LIMIT SELL {int(trade_volume)} {product} at {best_bid} "
                 logger.print(summary)
         #adjust by doing remaining trades with buy and sell orders 
 
